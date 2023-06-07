fabb29eff35c09cc74044b4e1c0f431af53a32fd
prince-sunsara <princesunasara977@gmail.com>
2023-06-07 10:02:41 +0530

    level 1 security

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 //jshint esversion:6
+
+
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -14,13 +17,17 @@ app.set('view engine', 'ejs');
 // mongoDB work
 mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 
-const useSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     email: String,
     password: String,
 });
 
-const User = new mongoose.model("User", useSchema);
 
+// security purpose (encryption)
+const secret = "Thisissomesecretekey";
+userSchema.plugin(encrypt, { secret : secret , encryptedFields: ['password']});
+
+const User = new mongoose.model("User", userSchema);
 
 // home route 
 app.get("/", (req, res) => {
